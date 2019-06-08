44683250c2e9329375a1a1dcbda65fcfa8fc5a3f
RATHOREDPS03 <rathoredpsingh03@gmail.com>
2019-06-09 00:58:46 +0530

    Update default.rb

--- mongodb3/recipes/default.rb
+++ mongodb3/recipes/default.rb
@@ -198,5 +198,4 @@ service 'mongod' do
   action :enable
   subscribes :restart, "template[#{node['mongodb3']['mongod']['config_file']}]", :delayed
   subscribes :restart, "template[#{node['mongodb3']['config']['mongod']['security']['keyFile']}", :delayed
-  #>>>>>>> 59123b7291922651a404c2a0fef43fc9bb9029c0
-end
+ #>>>>>>> 59123b7291922651a404c2a0fef43fc9bb9029c0
