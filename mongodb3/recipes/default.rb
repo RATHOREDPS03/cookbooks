26aa9a3bd8bb738566ecd5ec3dc195c3d013ea8e
RATHOREDPS03 <rathoredpsingh03@gmail.com>
2019-06-10 12:13:30 +0530

    Update default.rb

--- mongodb3/recipes/default.rb
+++ mongodb3/recipes/default.rb
@@ -2,11 +2,7 @@
 # Cookbook Name:: mongodb3
 # Recipe:: default
 #
-#<<<<<<< HEAD
 # Copyright 2015, UrbanLadder
-#=======
-# Copyright 2015, Sunggun Yu
-#>>>>>>> 59123b7291922651a404c2a0fef43fc9bb9029c0
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
@@ -19,33 +15,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-##
-#<<<<<<< HEAD
+#
 include_recipe 'mongodb3::install'
 
 directory node['mongodb3']['mongod']['log_dir'] do
-#=======
-
-include_recipe 'mongodb3::package_repo'
-
-# Install MongoDB package
-install_package = %w(mongodb-org-server mongodb-org-shell mongodb-org-tools)
-
-install_package.each do |pkg|
-  package pkg do
-    version node['mongodb3']['package']['version']
-    case node['platform_family']
-      when 'debian'
-        # bypass dpkg errors about pre-existing init or conf file
-        options '-o Dpkg::Options::="--force-confold" --force-yes'
-    end
-    action :install
-  end
-end
-
-# Create the db path if not exist.
-directory node['mongodb3']['config']['mongod']['storage']['dbPath'] do
-#>>>>>>> 59123b7291922651a404c2a0fef43fc9bb9029c0
   owner node['mongodb3']['user']
   group node['mongodb3']['group']
   mode '0755'
@@ -53,13 +26,8 @@ directory node['mongodb3']['config']['mongod']['storage']['dbPath'] do
   recursive true
 end
 
-#<<<<<<< HEAD
 # Create the db path if not exist.
 directory node['mongodb3']['config']['mongod']['storage']['dbPath'] do
-#=======
-# Create the systemLog directory.
-directory File.dirname(node['mongodb3']['config']['mongod']['systemLog']['path']).to_s do
-#>>>>>>> 59123b7291922651a404c2a0fef43fc9bb9029c0
   owner node['mongodb3']['user']
   group node['mongodb3']['group']
   mode '0755'
@@ -67,29 +35,6 @@ directory File.dirname(node['mongodb3']['config']['mongod']['systemLog']['path']
   recursive true
 end
 
-#<<<<<<< HEAD
-#=======
-unless node['mongodb3']['config']['key_file_content'].to_s.empty?
-  # Create the key file if it is not exist
-  key_file = node['mongodb3']['config']['mongod']['security']['keyFile']
-
-  # Create the directory for key file
-  directory File.dirname(key_file).to_s do
-    action :create
-    owner node['mongodb3']['user']
-    group node['mongodb3']['group']
-    recursive true
-  end
-
-  file key_file do
-    content node['mongodb3']['config']['key_file_content']
-    mode '0600'
-    owner node['mongodb3']['user']
-    group node['mongodb3']['group']
-  end
-end
-
-#>>>>>>> 59123b7291922651a404c2a0fef43fc9bb9029c0
 # Update the mongodb config file
 template node['mongodb3']['mongod']['config_file'] do
   source 'mongodb.conf.erb'
@@ -100,7 +45,6 @@ template node['mongodb3']['mongod']['config_file'] do
   helpers Mongodb3Helper
 end
 
-#<<<<<<< HEAD
 template "/etc/init/mongod.conf" do
   source "mongod.upstart.erb"
   mode '644'
@@ -131,72 +75,4 @@ end
 service 'mongod' do
   provider Chef::Provider::Service::Upstart
   action [ :stop, :start ]
-#=======
-# Disable Transparent Huge Pages (THP)
-# https://docs.mongodb.com/manual/tutorial/transparent-huge-pages/
-cookbook_file '/etc/init.d/disable-transparent-hugepages' do
-  source 'disable-transparent-hugepages'
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-  only_if {
-    node['mongodb3']['mongod']['disable-transparent-hugepages']
-  }
-end
-
-case node['platform']
-  when 'ubuntu'
-    if node['platform_version'].to_f >= 15.04
-      cookbook_file '/lib/systemd/system/disable-transparent-hugepages.service' do
-        source 'disable-transparent-hugepages.service'
-        owner 'root'
-        group 'root'
-        mode '0655'
-        action :create
-        only_if {
-          node['mongodb3']['mongod']['disable-transparent-hugepages']
-        }
-      end
-    end
-end
-
-service 'disable-transparent-hugepages' do
-  case node['platform']
-    when 'ubuntu'
-      if node['platform_version'].to_f >= 15.04
-        provider Chef::Provider::Service::Systemd
-      end
-  end
-  action [ :enable, :start ]
-  only_if {
-    node['mongodb3']['mongod']['disable-transparent-hugepages']
-  }
-end
-
-# Create the mongod.service file
-case node['platform']
-  when 'ubuntu'
-    template '/lib/systemd/system/mongod.service' do
-      source 'mongod.service.erb'
-      mode 0644
-      only_if { node['platform_version'].to_f >= 15.04 }
-    end
-end
-
-# Start the mongod service
-service 'mongod' do
-  case node['platform']
-    when 'ubuntu'
-      if node['platform_version'].to_f >= 15.04
-        provider Chef::Provider::Service::Systemd
-      elsif node['platform_version'].to_f >= 14.04
-        provider Chef::Provider::Service::Upstart
-      end
-  end
-  supports :start => true, :stop => true, :restart => true, :status => true
-  action :enable
-  subscribes :restart, "template[#{node['mongodb3']['mongod']['config_file']}]", :delayed
-  subscribes :restart, "template[#{node['mongodb3']['config']['mongod']['security']['keyFile']}", :delayed
-#>>>>>>> 59123b7291922651a404c2a0fef43fc9bb9029c0
 end
